8d2f79741a94cee0bd576450bdb91878200b620d
stefanorausch <stefanorausch@6b6dcee1-3708-0410-978a-85fc933b64b3>
2006-05-31 19:39:24 +0000

    Added the new Form Editor version 0.5.043 coded by Rubem.
    BUG FIXES
    
    - include/wb_generic.inc.php: functions generate_ini and parse_ini do now handle double quotes correctly; fixed missing escaping of the '$' character
    
    - include/wb_windows.inc.php: function wb_set_selected now processes check boxes in menu items as it should;  wb_get_text() now returns an array with the whole contents of a ListView control, as stated in the manual - did return null instead
    
    ADDITIONAL ENHANCEMENTS
    
    - examples/hyperlink.phpw: changed to show "finger" cursor and underlined text
    
    - resources/wb.ico: added new WinBinder icon
    
    - Added Rubem's PHP code example how to print directly from a WinBinder enabled application.

--- trunk/phpcode/include/wb_windows.inc.php
+++ trunk/phpcode/include/wb_windows.inc.php
@@ -130,9 +130,19 @@ function wb_get_text($ctrl, $item=null, $subitem=null)
 		} else {					// NULL item
 
 			$sel = wb_get_selected($ctrl);
-			if($sel === null)
-				return null;
-			else {
+			if($sel === null) {		// Returns the entire table
+				$items = array();
+				for($i = 0;;$i++) {
+					$item = wbtemp_get_listview_text($ctrl, $i);
+					$all = '';
+					foreach($item as $col)
+						$all .= $col;
+					if($all == '')
+						break;
+					$items[] = $item;
+				}
+				return $items ? $items : null;
+			} else {
 				$items = array();
 				foreach($sel as $row)
 					$items[] = wbtemp_get_listview_text($ctrl, $row);
@@ -317,7 +327,7 @@ function wb_set_selected($ctrl, $selitems, $selected=TRUE)
 			break;
 
 		case Menu:
-			return wbtemp_set_menu_item_selected($ctrl, $selected);
+			return wbtemp_set_menu_item_checked($ctrl, $selitems, $selected);
 
 		case TabControl:
 			wbtemp_select_tab($ctrl, (int)$selitems);
